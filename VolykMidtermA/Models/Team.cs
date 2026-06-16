2571a4fd017196232c443566f78ad9dccb7cb367
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-06-16 13:17:22 -0400

    4

---
+++ VolykMidtermA/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿namespace VolykMidtermA.Models
+{
+    public class Team
+    {
+        // a.
+        public int TeamId { get; set; }
+
+        // b.
+        public string Country { get; set; }
+
+        // c.
+        public string Group { get; set; }
+
+        // d.
+        public int Ranking { get; set; }
+
+        // e.
+        public DateTime FirstMatch { get; set; }
+    }
+}
