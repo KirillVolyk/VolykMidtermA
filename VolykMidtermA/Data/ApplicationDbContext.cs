45f8ddaf64665b354bb90d9c17264e9f224a9a3b
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-06-16 13:07:38 -0400

    Add project files.

---
+++ VolykMidtermA/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VolykMidtermA.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
